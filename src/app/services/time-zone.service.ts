e7c95daf1e7f3036b5e6b807a29ef8bb6aaccf88
Adnan <ad.haybe1@gmail.com>
2023-02-09 21:49:09 +0000

    Added environment variables

--- src/app/services/time-zone.service.ts
+++ src/app/services/time-zone.service.ts
@@ -1,23 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimeZoneService {
   
-  private API_KEY = '7PII7ID7JEYG';
-
   constructor(private http: HttpClient) { }
 
   getTimeZone(latitude: number, longitude: number): Observable<any> {
-    const url = `http://api.timezonedb.com/v2.1/get-time-zone?key=${this.API_KEY}&format=json&by=position&lat=${latitude}&lng=${longitude}`;
+    const url = `http://api.timezonedb.com/v2.1/get-time-zone?key=${environment.timezoneAPIKey}&format=json&by=position&lat=${latitude}&lng=${longitude}`;
     return this.http.get(url);
   }
-
-
-
+  
   getTimeZoneOffset() {
     return Intl.DateTimeFormat().resolvedOptions().timeZone;
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  timezoneAPIKey: 'R6N243K7AJQ1'
 };
